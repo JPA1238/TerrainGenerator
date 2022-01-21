e3e36fb070b58e7823682b049d8a55db868a0837
JPA1238 <aaronvb@outlook.com>
2022-01-21 22:24:57 +0100

    Initial commit

---
+++ src/main/java/com/jpa/terrainGenerator.java
@@ -0,0 +1,13 @@
+package com.jpa;
+
+/*
+* @author : JPA1238
+* @version : 0.1
+* @version : 0.1-alpha
+*/
+
+public class terrainGenerator {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
