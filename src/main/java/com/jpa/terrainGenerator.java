906aec6450d6c8ff66f92dba5030526b2d070181
JPA1238 <aaronvb@outlook.com>
2022-02-07 09:58:07 +0100

    Changed version

--- src/main/java/com/jpa/terrainGenerator.java
+++ src/main/java/com/jpa/terrainGenerator.java
@@ -2,14 +2,21 @@ package com.jpa;
 
 public class terrainGenerator {
     public static void main(String[] args) {
-
-
-        heightmap belgiumHM = new heightmap(50, 4, 1, 1);
         // heightmap worldHM = new heightmap(90, -180, 180, 360);
+
+        /** 
+        heightmap belgiumHM = new heightmap(50, 4, 1, 1);
 
         model belgium3D = new model(belgiumHM, 20, 20);
         belgium3D.settings.put("baseHeight", "1");
         belgium3D.generate();
-        belgium3D.export("data/STLs/");
+        belgium3D.export("data/STLs/"); 
+        */
+
+        heightmap everestHM = new heightmap(27, 86, 1, 1);
+
+        model everest3D = new model(everestHM, 20, 20);
+        everest3D.generate();
+        everest3D.export("data/STLs/");
     }
 }
